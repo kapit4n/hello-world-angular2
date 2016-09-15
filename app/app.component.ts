d67444c67623532e150cb45ca8d407d87488a95e
Luis Arce <luis.arce@jalasoft.com>
2016-09-15 14:20:17 -0400

    Use varuables in the angular 2 components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+  			<h1>
+  			My First Angular 2 App This {{varValue}} {{hero.name}}
+  			</h1>
+  			 `
 })
-export class AppComponent { }
+
+export class AppComponent { 
+	varValue = "Hello World!!";
+	hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
