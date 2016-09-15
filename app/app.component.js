c37700edd9f58f62681850cf6fa62aef4f425c9e
Luis Arce <luis.arce@jalasoft.com>
2016-09-15 14:26:15 -0400

    Updated component to use formsModule

--- app/app.component.js
+++ app/app.component.js
@@ -11,15 +11,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.varValue = "Hello World!!";
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: "\n            <h1>{{title}}</h1>\n                <h2>{{hero.name}} details!</h2>\n                <div><label>id: </label>{{hero.id}}</div>\n                <div>\n                  <label>name: </label>\n                  <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n                </div>\n             "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  			<h1>
-  			My First Angular 2 App This {{varValue}} {{hero.name}}
-  			</h1>
-  			 `
+            <h1>{{title}}</h1>
+                <h2>{{hero.name}} details!</h2>
+                <div><label>id: </label>{{hero.id}}</div>
+                <div>
+                  <label>name: </label>
+                  <input [(ngModel)]="hero.name" placeholder="name">
+                </div>
+             `
 })
 
 export class AppComponent { 
-	varValue = "Hello World!!";
-	hero: Hero = {
-	  id: 1,
-	  name: 'Windstorm'
-	};
+    varValue = "Hello World!!";
+    hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
 }
 
 export class Hero {
